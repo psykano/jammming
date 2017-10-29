112b128b42f52e080b7c98471733570bb40cb7b6
Chris Johns <chrisjohns@gmail.com>
2017-10-29 00:42:22 -0400

    render hard-coded tracks

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { searchResults: [
+      { name: 'name1', artist: 'artist1', album: 'album1' },
+      { name: 'name2', artist: 'artist2', album: 'album2' }
+    ]};
+  }
+
   render() {
     return (
       <div>
@@ -10,6 +21,7 @@ class App extends Component {
           {/* Add a SearchBar component */}
           <div className="App-playlist">
             {/* Add a SearchResults component */}
+            <SearchResults searchResults={this.state.searchResults} />
             {/* Add a Playlist component */}
           </div>
         </div>
